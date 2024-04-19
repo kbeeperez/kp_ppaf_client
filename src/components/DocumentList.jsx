51ab8de2082bd1207390f4f0c1f2bc0b39ba7c3f
Katherine Perez <kpere15@lsu.edu>
2024-04-19 07:18:22 -0500

    added analyses to documents page.

--- src/components/DocumentList.jsx
+++ src/components/DocumentList.jsx
@@ -5,10 +5,12 @@ import DocumentStatsCard from "./DocumentStatsCard"
 import { Form } from "react-router-dom"
 import { useToggle } from "@mantine/hooks"
 import { IconMoodSad } from "@tabler/icons-react"
+import AnalysesCard from "./AnalysesCard"
 
 export default function DocumentList() {
     if (localStorage.getItem("token")) {
         let [documents, setDocuments] = useState([])
+        let [analyses, setAnalyses] = useState([])
         let [user, setUser] = useState(undefined)
 
         let [toggleState, toggle] = useToggle();
@@ -19,6 +21,10 @@ export default function DocumentList() {
         useEffect(() => {
             axios.get("/document/",).then((res) => {
                 setDocuments(res.data)
+            })
+
+            axios.get("/analysis/").then((res) => {
+                setAnalyses(res.data)
             })
 
             axios.get("/user/me").then((res)=>{
@@ -65,9 +71,15 @@ export default function DocumentList() {
                     </Form>
                 </Container>
                 <Title order={2} py="md" c="black">{user?.name}'s Documents</Title>
-                <Grid>
+                <Grid mb={"lg"}>
                     {documents.length >= 1 ? documents.map((item) => { return <GridCol span={{ base: 12, xs: 4 }}><DocumentStatsCard data={item} refreshHandler={toggle}/></GridCol> }) : (<GridCol span={{ base: 12, xs: 12 }}><Paper withBorder p="sm"><Text fz="xl" c="dimmed">It's lonely in here... <IconMoodSad style={{verticalAlign: "bottom"}}/></Text><Text fz="md" c="dimmed">Add a new document to get started</Text></Paper></GridCol>)}
                 </Grid>
+                <Divider/>
+                <Title order={2} py="md" c="black">{user?.name}'s Analyses</Title>
+                <Grid>
+                    {analyses.length >= 1 ? analyses.map((item) => { return <GridCol span={{ base: 12, xs: 4 }}><AnalysesCard data={item} refreshHandler={toggle}/></GridCol> }) : (<GridCol span={{ base: 12, xs: 12 }}><Paper withBorder p="sm"><Text fz="xl" c="dimmed">It's lonely in here... <IconMoodSad style={{verticalAlign: "bottom"}}/></Text><Text fz="md" c="dimmed">Create a new analysis to get started!</Text></Paper></GridCol>)}
+                </Grid>
+
             </Container>)
     }
 }
